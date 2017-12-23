0f2158ffa81b3435e2ff248894f33bf0d89e211e
Sean Oberoi <xseano@yandex.com>
2017-12-22 18:06:29 -0800

    add main bot server class

---
+++ bot.js
@@ -0,0 +1,75 @@
+const Discord = require('discord.js');
+const os = require('os');
+const botLoop = require('node-gameloop');
+const CommandHandler = require('./lib/CommandHandler');
+
+class Bot
+{
+    /*
+    Initializes a new instance of the Bot
+    */
+
+    constructor(parent)
+    {
+
+        // Create Bot
+        this.parent = parent
+        this.bot = new Discord.Client();
+
+        // Bot Variables
+        this.id = 0;
+        this.tick = 0;
+
+        // Command Handler
+        this.commands = new CommandHandler(this);
+
+    }
+
+    /*
+    Starts the Bot
+    */
+
+    start()
+    {
+
+        Logger.print(`Clash Bot <Build ${Config.Version}>`.green.bold);
+
+        // Logger
+        Logger.prompt(this.commands.handleCommand.bind(this.commands), Config.Logger.Prompt);
+
+        // Good solution for minimal CPU usage
+        this.loop = botLoop.setGameLoop(this.botLoop.bind(this), Config.Server.Tick);
+
+        // System info debug
+        Logger.info(`OS: ${os.platform()}`);
+        Logger.info(`CPU: ${os.cpus()[0]["model"]}`);
+
+        Logger.info(`botLoop running at ${Config.Server.Tick} ms/tick`);
+
+        Logger.info(`Took ${process.uptime()} seconds to start`);
+        Logger.info(`Type 'help' or '?' for commands \n`)
+
+    }
+
+    onConnect()
+    {
+        // TODO
+    }
+
+    sendMsg(message)
+    {
+        // TODO
+    }
+
+    botLoop(delta)
+    {
+        if (this.tick >= 10)
+        {
+            this.tick = 0;
+        }
+
+        this.tick++;
+    }
+}
+
+module.exports = Bot;
--- start.js
+++ start.js
@@ -3,6 +3,7 @@ global.Config   = require('./conf/main.json');
 global.readline = require('readline');
 global.Logger   = require('./lib/Logger');
 global.Colors   = require('colors');
+global.Bot      = require('./Bot');
 global.rl       = readline.createInterface(
     {
         input:  process.stdin,
@@ -15,7 +16,8 @@ Logger.print("Loading Dependencies...".green.dim);
 Logger.start();
 
 // Start Bot
-// TODO
+var bot = new Bot(this);
+bot.start();
 
 // Error handling
 process.on('uncaughtException', (error) => Logger.error(error));
