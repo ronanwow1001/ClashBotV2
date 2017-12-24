b543ebd8ae5c1446dfd05dd0f32670cf56096c6c
Sean Oberoi <xseano@yandex.com>
2017-12-24 14:44:42 -0800

    un/+mute user notifications

--- handlers/MessageHandler.js
+++ handlers/MessageHandler.js
@@ -347,6 +347,21 @@ class MessageHandler
         {
             guildUser.addRole(role);
             message.reply(`I've muted the user: ${user.username}`);
+
+            const embed = new Discord.RichEmbed()
+              .setDescription('**You\'ve been muted in the Corporate Clash discord for moderation purposes.**\n')
+              .setAuthor(user.username, this.getAvatar(message))
+
+              .setColor('#FF0000')
+              .setFooter("© Corporate Clash 2017-2018")
+
+              .setTimestamp();
+
+             user.send(
+                 {
+                     embed
+                 }
+             );
         }
         else
         {
@@ -360,6 +375,21 @@ class MessageHandler
         {
             guildUser.removeRole(role);
             message.reply(`I unmuted the user: ${user.username}`);
+
+            const embed = new Discord.RichEmbed()
+              .setDescription('**Your mute has been lifted in the Corporate Clash discord.**\n')
+              .setAuthor(user.username, this.getAvatar(message))
+
+              .setColor('#FF0000')
+              .setFooter("© Corporate Clash 2017-2018")
+
+              .setTimestamp();
+
+             user.send(
+                 {
+                     embed
+                 }
+             );
         }
         else
         {
